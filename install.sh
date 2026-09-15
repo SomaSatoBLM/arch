20a3dad7d75357896d9931a00dc1cd7d3ba4a758
SomaSato <2262954+SomaSatoBLM@users.noreply.github.com>
2026-09-14 21:00:41 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ pacstrap -i /mnt --noconfirm base base-devel linux linux-headers git nano fish \
     pavucontrol ntfs-3g openssh python-pip wget reflector \
     nvidia-open nvidia-open-lts lib32-nvidia-utils nvidia-utils lib32-opencl-nvidia nvidia-settings  \
     lib32-vkd3d vkd3d opencl-nvidia libvdpau libxnvctrl egl-wayland nvtop linux-firmware-nvidia \
-    ppsspp nvtop vulkan-tools wine-staging lutris winetricks ffnvcodec-headers \
+    ppsspp steam nvtop vulkan-tools wine-staging lutris winetricks ffnvcodec-headers \
     plasma-meta kde-applications-meta packagekit-qt6 fwupd flatpak linux-firmware-realtek \
     libreoffice-fresh vivaldi vivaldi-ffmpeg-codecs mtools linux-firmware-intel \
     jre8-openjdk jre11-openjdk jre17-openjdk jre21-openjdk jre-openjdk wireless-regdb \
